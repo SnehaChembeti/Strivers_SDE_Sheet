3d835e997436288c6a9debfdd0544d57e6a3b155
Muni Sneha Sai Chembeti <91887869+SnehaChembeti@users.noreply.github.com>
2022-10-01 19:17:03 +0530

    Add files via upload

--- Day 6 Linked List - II/6. Flattening a LinkedList.cpp
+++ Day 6 Linked List - II/6. Flattening a LinkedList.cpp
@@ -1,3 +1,12 @@
+/*
+Given a Linked List of size N, where every node represents a sub-linked-list and contains two pointers:
+(i) a next pointer to the next node,
+(ii) a bottom pointer to a linked list where this node is head.
+Each of the sub-linked-list is in sorted order.
+Flatten the Link List such that all the nodes appear in a single level while maintaining the sorted order.
+Note: The flattened list will be printed using the bottom pointer instead of next pointer. For more clearity have a look at the printList() function in the driver code.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
